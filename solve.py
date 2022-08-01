508e3f5d9c44fcff7605b99a842eb9f5d6edac75
mmalla <mmalla@imcc.com>
2022-08-01 11:54:20 -0500

    fixed grid size

--- solve.py
+++ solve.py
@@ -15,17 +15,18 @@ class Solve:
         for i in inference_result.xyxy[0]:
             class_name = self.names[int(i[5])]
 
+            # images are 288 x 288
             tile_row, tile_col = 0, 0
             if item == class_name:
                 if size == 3:
-                    # squares are 125 x 125
-                    tile_col = math.ceil(float(i[0]) / 125.0)
-                    tile_row = math.ceil(float(i[1]) / 125.0)
-
-                if size == 4:
-                    # squares are 95x95
+                    # squares are 95 x 95
                     tile_col = math.ceil(float(i[0]) / 95.0)
                     tile_row = math.ceil(float(i[1]) / 95.0)
+
+                if size == 4:
+                    # squares are 70x70
+                    tile_col = math.ceil(float(i[0]) / 70.0)
+                    tile_row = math.ceil(float(i[1]) / 70.0)
 
                 results.append({"tile_col": tile_col, "tile_row": tile_row, "class_name": class_name})
 
